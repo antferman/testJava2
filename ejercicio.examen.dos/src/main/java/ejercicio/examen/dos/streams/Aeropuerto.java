b610d307a231fa5f3102b38f424c323bf5e16dd9
Antonio Fernández <afmanzano@indra.es>
2021-11-22 11:38:42 +0100

    Ejercicios streams terminados

---
+++ ejercicio.examen.dos/src/main/java/ejercicio/examen/dos/streams/Aeropuerto.java
@@ -0,0 +1,55 @@
+package ejercicio.examen.dos.streams;
+
+import java.util.List;
+
+public class Aeropuerto {
+
+	private Integer id;
+	private String ciudad;
+	private String name;
+	
+	public Aeropuerto() {
+		
+	}
+
+	
+	public Aeropuerto(Integer id, String ciudad, String name) {
+		this.id = id;
+		this.ciudad = ciudad;
+		this.name = name;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	@Override
+	public String toString() {
+		return "Aeropuerto [id=" + id + ", ciudad=" + ciudad + ", name=" + name + "]";
+	}
+	
+	
+}
---
+++ ejercicio.examen.dos/src/main/java/ejercicio/examen/dos/streams/Billete.java
@@ -0,0 +1,54 @@
+package ejercicio.examen.dos.streams;
+
+import java.time.LocalDate;
+
+public class Billete {
+
+	private Integer id;
+	private Double precio;
+	private Pasajero pasajero;
+	
+	public Billete() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Pasajero getPasajero() {
+		return pasajero;
+	}
+
+	public void setPasajero(Pasajero pasajero) {
+		this.pasajero = pasajero;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Billete [id=" + id + ", precio=" + precio + ", pasajero=" + pasajero + "]";
+	}
+
+	public Billete(Integer id, Double precio, Pasajero pasajero) {
+		this.id = id;
+		this.precio = precio;
+		this.pasajero = pasajero;
+
+	}
+	
+	
+}
